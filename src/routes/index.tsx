ac124217ad6b11072531b2de0473aa8dac684b75
electricman07 <glenp@telus.net>
2026-02-25 16:49:08 -0800

    made minor changes to styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,18 +5,18 @@ export const Route = createFileRoute("/")({ component: App });
 function App() {
   return (
     <div className="min-h-screen bg-[#c58f60]">
-      <main className="container ">
+      <main className="container w-screen">
         <header className="header flex items-center justify-center gap-12 py-16 px-6 max-w-7xl mx-auto">
-          <div className="text-container text-center h-screen">
-            <div className="text flex flex-col content-evenly items-center justify-center h-full">
+          <div className="text-container text-center ">
+            <div className="text flex flex-col content-evenly items-center justify-center h-full w-full">
               <h2 className="pre-header text-4xl m-1 mt-8">Hello!!! I'm</h2>
-              <h1 className="main-header text-8xl m-1 mt-16">Rylee</h1>
-              <h3 className="sub-header text-3xl ml-1 mt-16">
+              <h1 className="main-header text-8xl m-1 mt-20 ml-52">Rylee</h1>
+              <h3 className="sub-header text-2xl mt-20 ml-40">
                 the very friendly beagle
               </h3>
             </div>
           </div>
-          <div className="image-header mt-2 mr-4 mb-2 flex justify-center items-center">
+          <div className="image-header mt-2 mr-4 mb-2 flex justify-center items-center w-1/2">
             <img
               className="w100 h100 rounded-lg"
               src="/images/header.jpg"
