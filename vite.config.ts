77fd8d69926a8a074d6e47d03faeb8119646dcdc
electricman07 <glenp@telus.net>
2026-02-25 16:28:40 -0800

    modified for path to font

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   plugins: [
     devtools(),
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
-})
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: [
+        "..",
+        "/media/glenp/Samsung/Development/Projects/Client/Rylee2/node_modules/@fontsource/irish-grover/files/",
+      ],
+    },
+  },
+});
 
-export default config
+export default config;
