7c5ff79c2c05ab4db806d16069f74ac3a1c9d96e
electricman07 <glenp@telus.net>
2026-03-02 16:18:55 -0800

    created photos file

---
+++ src/addons/photos.ts
@@ -0,0 +1,99 @@
+interface Photo {
+  id: number;
+  src: string;
+  width: number;
+  height: number;
+}
+
+export const photos: Photo[] = [
+  {
+    id: 1,
+    src: "../../public/images/image_scroll/image-1.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 2,
+    src: "../../public/images/image_scroll/image-2.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 3,
+    src: "../../public/images/image_scroll/image-3.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 4,
+    src: "../../public/images/image_scroll/image-4.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 5,
+    src: "../../public/images/image_scroll/image-5.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 6,
+    src: "../../public/images/image_scroll/image-6.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 7,
+    src: "../../public/images/image_scroll/image-7.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 8,
+    src: "../../public/images/image_scroll/image-8.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 9,
+    src: "../../public/images/image_scroll/image-9.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 10,
+    src: "../../public/images/image_scroll/image-10.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 11,
+    src: "../../public/images/image_scroll/image-11.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 12,
+    src: "../../public/images/image_scroll/image-12.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 13,
+    src: "../../public/images/image_scroll/image-13.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 14,
+    src: "../../public/images/image_scroll/image-14.jpg",
+    width: 1060,
+    height: 800,
+  },
+  {
+    id: 15,
+    src: "../../public/images/image_scroll/image-15.jpg",
+    width: 1060,
+    height: 800,
+  },
+];
