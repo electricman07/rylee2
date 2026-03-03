cbb038c47200bc5b114853907da3b4ec152eb17d
electricman07 <glenp@telus.net>
2026-03-02 16:19:54 -0800

    added react-photo-album and yet-another-react-lightbox and configured

--- src/routes/images.tsx
+++ src/routes/images.tsx
@@ -1,9 +1,49 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { MasonryPhotoAlbum } from "react-photo-album";
+import InfiniteScroll from "react-photo-album/scroll";
+import "react-photo-album/masonry.css";
+import { photos } from "../addons/photos";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
-export const Route = createFileRoute('/images')({
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import { useState } from "react";
+
+const fetchPhotos: number = 10;
+
+export const Route = createFileRoute("/images")({
   component: RouteComponent,
-})
+});
+
+async function fetchMorePhotos(index: number) {
+  // Implement your photo fetching logic here
+  // Return the next batch of photos based on the index
+  return [];
+}
 
 function RouteComponent() {
-  return <div>Hello "/images"!</div>
+  const [index, setIndex] = useState(-1);
+
+  return (
+    <>
+      <MasonryPhotoAlbum
+        photos={photos}
+        padding={12}
+        onClick={({ index }) => setIndex(index)}
+      />
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
+    </>
+  );
 }
